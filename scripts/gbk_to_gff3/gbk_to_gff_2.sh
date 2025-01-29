a51cabf6a09f391e870c92f347dd32239b9bf473
eliott-tempez <tempez.el@gmail.com>
2025-01-29 16:12:58 +0100

    Update file to convert 3 last gbk files

--- scripts/gbk_to_gff3/gbk_to_gff_2.sh
+++ scripts/gbk_to_gff3/gbk_to_gff_2.sh
@@ -12,5 +12,5 @@ for file in ./*correction.gbk; do
     output_file="../gff3/${base_name}.gff3"
     echo "${base_name}:" >> "$log_script"
     echo "${base_name}:" >> "$err_script"
-    python3 /home/eliott.tempez/Documents/archaea_data/convert_genbank_to_gff3.py -i "$file" -o "$output_file" >> "$log_script" 2>> "$err_script"
+    python /home/eliott.tempez/Documents/archaea_data/gbk_to_gff/convert_genbank_to_gff3.py -i "$file" -o "$output_file" >> "$log_script" 2>> "$err_script"
 done
