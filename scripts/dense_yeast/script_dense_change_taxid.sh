31e57eef3964f790e1bbe44753cb110c06aee306
eliott-tempez <tempez.el@gmail.com>
2025-01-31 13:42:22 +0100

    Add data importation to script

--- scripts/dense_yeast/script_dense_change_taxid.sh
+++ scripts/dense_yeast/script_dense_change_taxid.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 #PBS -N dense_change_taxid
 #PBS -q bim
 #PBS -l ncpus=32 -l host=node04 -l mem=128gb -l walltime=240:00:00
@@ -8,9 +8,16 @@
 # Set up environment
 echo "Activating conda environment" >> /home/eliott.tempez/dense_change_taxid_output.log
 source /home/eliott.tempez/miniconda3/bin/activate dense
+cd /datas/ELIOTT/
+
+# Get data
+mkdir /datas/ELIOTT/result_dense_yeast_3/
+rm -rf /datas/ELIOTT/yeast_data/*
+cp -r /store/EQUIPES/BIM/MEMBERS/eliott.tempez/yeast_data/dense_change_taxid/* /datas/ELIOTT/yeast_data/
+echo "Data imported" >> /home/eliott.tempez/dense_change_taxid_output.log
+
 
 # Run DENSE
-cd /datas/ELIOTT/
 echo "Running Nextflow" >> /home/eliott.tempez/dense_change_taxid_output.log
 nextflow run /home/eliott.tempez/dense -profile singularity -c /home/eliott.tempez/params_change_taxid.config >> /home/eliott.tempez/dense_change_taxid_output.log 2>> /home/eliott.tempez/dense_change_taxid_error.log
 EXIT_CODE=$?
