1f428b568bccc3d319d65586b3a24decf5cc9ca6
eltempez <tempez.el@gmail.com>
2025-02-19 19:14:40 +0100

    Fix typo

--- scripts/explore_dense_result/get_extended_TRGs.sh
+++ scripts/explore_dense_result/get_extended_TRGs.sh
@@ -2,19 +2,19 @@
 #### This script aims to extract all extended TRG faa files from the tmp files created by dense ####
 
 # Get all file names
-FASTA_FILES=$(ls /store/EQUIPES/BIM/MEMBERS/eliott.tempez/archaea_data/complete_122/fasta_renamed/*.fa)
+FASTA_FILES=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/archaea_data/complete_122/fasta_renamed/*.fa
 cd /datas/ELIOTT/scripts/work/
-mkdir extended_CDS_list
+mkdir -p extended_CDS_list
 
 for FILE in $FASTA_FILES
 do
     # Extract the basename without the extension
-    BASENAME=$(basename $FILE .fa)
+    BASENAME_ORG=$(basename $FILE .fa)
     # File name to look for
-    TARGET_FILE="TRG_multielongated_blastp_${BASENAME}_CDS_elongated.out"
+    TARGET_FILE="TRG_multielongated_blastp_"$BASENAME_ORG"_CDS_elongated.out"
     # Run ls and get folder name for first match
     TARGET_FILEPATH=$(ls */*/$TARGET_FILE | head -n 1)
     TARGET_FOLDER=$(dirname $TARGET_FILEPATH)
     # Copy the TRH file to the extended_CDS_list folder
-    cp $TARGET_FOLDER/TRG_multielongated.faa extended_CDS_list/TRG_multielongated_$(BASENAME).faa
+    cp -L $TARGET_FOLDER"/TRG_multielongated.faa extended_CDS_list/TRG_multielongated_"$BASENAME_ORG".faa"
 done
