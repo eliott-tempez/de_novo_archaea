90f4262f7d778b52d39181dd5bc96b0b9cb347d7
eliott-tempez <tempez.el@gmail.com>
2025-02-26 09:50:11 +0100

    Change colors

--- scripts/calculate_conservation/map_gradient_tree.R
+++ scripts/calculate_conservation/map_gradient_tree.R
@@ -2,11 +2,15 @@ library(ggplot2)
 library(ggtree)
 library(ape)
 library(ggnewscale)
+ggsave <- function(..., bg = "white", width = 1000, height = 1000, units = "px", dpi = 100) {
+  ggplot2::ggsave(..., bg = bg, width = width, height = height, units = units, dpi = dpi)
+}
 
 # Filenames
 tree_file <- "/home/eliott.tempez/Documents/archaea_data/whole_tree.nwk"
 conservation_db_dir <- "/home/eliott.tempez/Documents/M2_Stage_I2BC/results/calculate_conservation/"
 focal_species <- "GCA_001433455@Thermococcus_barophilus_CH5"
+output_dir <- "/home/eliott.tempez/Documents/M2_Stage_I2BC/results/calculate_conservation/"
 
 # Get the conservation db
 conservation_db_file <- paste0(conservation_db_dir, "conservation_df_", focal_species, ".tsv")
@@ -25,42 +29,46 @@ tree <- read.tree(tree_file)
 # Plot the tree
 p <- ggtree(tree, layout = "circular", branch.length = "none") +
   geom_tippoint(aes(subset = (label == focal_species)), shape = 23,
-                color = "black", fill = "red",
+                color = "black", fill = "yellow",
                 size = 3, stroke = 1, alpha = 1)
 
 # Add the heatmaps
 ## TRGs ##
 p <- p + new_scale_fill()
 p <- gheatmap(p, conservation_db_p[, "n_trg", drop = FALSE],
-              width = .05, colnames = FALSE) +
-  scale_fill_gradient(low = "white", high = "#0004ff", name = "TRGs",
-                      guide = guide_colorbar(order = 1))
-## CDSs ##
-p <- p + new_scale_fill()
-p <- gheatmap(p, conservation_db_p[, "n_cds", drop = FALSE], offset = 1,
-              width = .05, colnames = FALSE) +
-  scale_fill_gradient(low = "white", high = "#be5c00", name = "CDS",
-                      guide = guide_colorbar(order = 2))
+        width = .05, colnames = FALSE) +
+  scale_fill_gradient(low = "white", high = "red", name = "TRGs",
+            guide = guide_colorbar(order = 1))
 ## Noncoding ##
 p <- p + new_scale_fill()
-p <- gheatmap(p, conservation_db_p[, "n_noncoding", drop = FALSE], offset = 2,
+p <- gheatmap(p, conservation_db_p[, "n_noncoding", drop = FALSE], offset = 1,
               width = .05, colnames = FALSE) +
-  scale_fill_gradient(low = "white", high = "#4d00a5", name = "Noncoding",
-                      guide = guide_colorbar(order = 3))
+  scale_fill_gradient(low = "white", high = "#6b00b2", name = "Noncoding",
+                      guide = guide_colorbar(order = 2))
 ## Frame 1 ##
 p <- p + new_scale_fill()
-p <- gheatmap(p, conservation_db_p[, "n_f1", drop = FALSE], offset = 3,
+p <- gheatmap(p, conservation_db_p[, "n_f1", drop = FALSE], offset = 2,
               width = .05, colnames = FALSE) +
-  scale_fill_gradient(low = "white", high = "#bdba17", name = "+1",
-                      guide = guide_colorbar(order = 4))
+  scale_fill_gradient(low = "white", high = "#009E73", name = "+1",
+                      guide = guide_colorbar(order = 3))
 ## Frame 2 ##
 p <- p + new_scale_fill()
-p <- gheatmap(p, conservation_db_p[, "n_f2", drop = FALSE], offset = 4,
+p <- gheatmap(p, conservation_db_p[, "n_f2", drop = FALSE], offset = 3,
               width = .05, colnames = FALSE) +
-  scale_fill_gradient(low = "white", high = "#12a712", name = "+2",
+  scale_fill_gradient(low = "white", high = "#0072B2", name = "+2",
+                      guide = guide_colorbar(order = 4))
+## CDSs ##
+p <- p + new_scale_fill()
+p <- gheatmap(p, conservation_db_p[, "n_cds", drop = FALSE], offset = 4,
+              width = .05, colnames = FALSE) +
+  scale_fill_gradient(low = "white", high = "#e67b00", name = "CDS",
                       guide = guide_colorbar(order = 5))
+
 
 # Add the title
 p <- p + ggtitle(paste0("Conservation rate for\n", focal_species)) +
   theme(plot.title = element_text(hjust = 0.5, vjust = -10))
 p
+
+# Save the image
+ggsave(paste0(output_dir, "conservation_map_", focal_species, ".png"))
