0b3c5c56801b7ace94ab63d64d49b896ff4e77df
eltempez <tempez.el@gmail.com>
2025-02-28 21:54:04 +0100

    Run dense for all species

--- scripts/dense_archaea/run_dense.sh
+++ scripts/dense_archaea/run_dense.sh
@@ -2,15 +2,19 @@
 
 ########## Parameters ##########
 
-#PBS -N dense_archaea
-#PBS -q bim
-#PBS -l ncpus=32 -l host=node04 -l mem=128gb -l walltime=300:00:00
+# Get the species from the argument
+archaea=$SPECIES
 
 # Data files
 GENDIR=/datas/ELIOTT/archaea_data/genome/
+NR=/datas/NR/nr_2.0.13.dmnd
 TREE=/datas/ELIOTT/archaea_data/whole_tree.nwk
 TAXID_FILE=/datas/ELIOTT/archaea_data/taxid.csv
-NR=/datas/NR/nr_2.0.13.dmnd
+GENERA_OUTFILE=/datas/ELIOTT/archaea_data/genera/out/${archaea}/*_gene_ages.tsv
+OUT_DIR=/datas/ELIOTT/archaea_data/dense/${archaea}/
+# Log filenames
+LOG_OUTPUT=/home/eliott.tempez/dense_output_$archaea.log
+LOG_ERROR=/home/eliott.tempez/dense_error_$archaea.log
 
 # Set up environment
 source /home/eliott.tempez/miniconda3/bin/activate dense
@@ -20,44 +24,27 @@ cd /datas/ELIOTT/scripts/
 
 ########## Run Dense ##########
 
-# List of archaeas to iterate over
-declare -a archaeas=("GCA_001433455@Thermococcus_barophilus_CH5")
-# For each of them
-for archaea in "${archaeas[@]}"; do
-
-    # Log filenames
-    LOG_OUTPUT=/home/eliott.tempez/dense_output_${archaea}.log
-    LOG_ERROR=/home/eliott.tempez/dense_error_${archaea}.log
-    #PBS -o $LOG_OUTPUT
-    #PBS -e $LOG_ERROR
-
-    echo $archaea >> $LOG_OUTPUT
-    # Get taxid
-    TAXID=$(grep $archaea $TAXID_FILE | cut -f2 -d$'\t')
-    echo "Taxid: $TAXID" >> $LOG_OUTPUT
-    # Get filenames
-    OUT_DIR=/datas/ELIOTT/archaea_data/dense/${archaea}/
+echo $archaea >> $LOG_OUTPUT
+mkdir -p $OUT_DIR
 
 
-    # Run Dense
-    echo "Running Dense..." >> $LOG_OUTPUT
-    nextflow run /home/eliott.tempez/dense \
-        -profile singularity \
-        --max_cpus 32 \
-        --max_memory 128.GB \
-        --max_time 300.h \
-        --num_outgroups 2 \
-        --gendir $GENDIR \
-        --focal $archaea \
-        --tree $TREE \
-        --taxids $TAXID_FILE \
-        --genera_db $NR \
-        --trg_node Thermococcaceae \
-        --outdir $OUT_DIR >> $LOG_OUTPUT 2>> $LOG_ERROR
+# Run Dense
+echo "Running Dense..." >> $LOG_OUTPUT
+nextflow run /home/eliott.tempez/dense \
+    -profile singularity \
+    --max_cpus 8 \
+    --max_memory 32.GB \
+    --max_time 50.h \
+    --num_outgroups 2 \
+    --gendir $GENDIR \
+    --focal $archaea \
+    --tree $TREE \
+    --taxids $TAXID_FILE \
+    --genera_db $NR \
+    --trg_node Thermococcaceae \
+    --outdir $OUT_DIR >> $LOG_OUTPUT 2>> $LOG_ERROR
 
 
-    # Deactivate conda environment
-    conda deactivate
-    echo -e "Job completed successfully\n" >> $LOG_OUTPUT
-
-done
+# Deactivate conda environment
+conda deactivate
+echo -e "Job completed successfully\n" >> $LOG_OUTPUT
