d93af947518f0145fe91669ac423e496d9bec435
eliott-tempez <tempez.el@gmail.com>
2025-03-03 13:12:02 +0100

    Run dense on other node than 04

--- scripts/dense_archaea/run_dense.sh
+++ scripts/dense_archaea/run_dense.sh
@@ -6,26 +6,33 @@
 archaea=$SPECIES
 
 # Data files
-GENDIR=/datas/ELIOTT/archaea_data/genome/
-NR=/datas/NR/nr_2.0.13.dmnd
-TREE=/datas/ELIOTT/archaea_data/whole_tree.nwk
-TAXID_FILE=/datas/ELIOTT/archaea_data/taxid.csv
-GENERA_OUTFILE=/datas/ELIOTT/archaea_data/genera/out/${archaea}/*_gene_ages.tsv
-OUT_DIR=/datas/ELIOTT/archaea_data/dense/${archaea}/
+GENDIR=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/archaea_data/complete_122/gendir_for_dense/
+TREE=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/archaea_data/whole_tree.nwk
+TAXID_FILE=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/archaea_data/taxid.csv
+GENERA_OUTFILE=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/archaea_data/genera/out/${archaea}/*_gene_ages.tsv
+OUT_DIR=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/archaea_data/dense/${archaea}/
 # Log filenames
 LOG_OUTPUT=/home/eliott.tempez/dense_output_$archaea.log
 LOG_ERROR=/home/eliott.tempez/dense_error_$archaea.log
 
 # Set up environment
 source /home/eliott.tempez/miniconda3/bin/activate dense
-cd /datas/ELIOTT/scripts/
+mkdir -p /scratchlocal/$USER/$SLURM_JOBID
+cd /scratchlocal/$USER/$SLURM_JOBID
+# Copy all inputs
+cp -r $GENDIR .
+cp $TREE .
+cp $TAXID_FILE .
+cp $GENERA_OUTFILE .
+# Create temp output directory
+mkdir -p out/
 
 
 
 ########## Run Dense ##########
 
 echo $archaea >> $LOG_OUTPUT
-mkdir -p $OUT_DIR
+
 
 
 # Run Dense
@@ -33,18 +40,23 @@ echo "Running Dense..." >> $LOG_OUTPUT
 nextflow run /home/eliott.tempez/dense \
     -profile singularity \
     --max_cpus 8 \
-    --max_memory 16.GB \
+    --max_memory 32.GB \
     --max_time 10.h \
     --num_outgroups 2 \
-    --gendir $GENDIR \
+    --gendir gendir_for_dense/ \
     --focal $archaea \
-    --tree $TREE \
-    --taxids $TAXID_FILE \
-    --genera_out $GENERA_OUTFILE \
+    --tree whole_tree.nwk \
+    --taxids taxid.csv \
+    --genera_out ${archaea}_gene_ages.tsv \
     --trg_node Thermococcaceae \
-    --outdir $OUT_DIR >> $LOG_OUTPUT 2>> $LOG_ERROR
+    --outdir out/ >> $LOG_OUTPUT 2>> $LOG_ERROR
 
+
+# Copy output
+mkdir -p $OUT_DIR
+cp -r out/* $OUT_DIR
 
 # Deactivate conda environment
+rm -r /scratchlocal/$USER/$SLURM_JOBID
 conda deactivate
 echo -e "Job completed successfully\n" >> $LOG_OUTPUT
