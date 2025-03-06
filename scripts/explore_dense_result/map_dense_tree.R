f6f8f5985d379937d5d18f64915fd7a2cad42461
eliott-tempez <tempez.el@gmail.com>
2025-03-06 17:27:25 +0100

    Map the tree for a giver trg in the trg match matrix

--- scripts/explore_dense_result/map_dense_tree.R
+++ scripts/explore_dense_result/map_dense_tree.R
@@ -1,30 +1,41 @@
+library(ggplot2)
 library(ggtree)
 library(ape)
+library(ggnewscale)
 
-TRG_FILE <- "/home/eliott.tempez/Documents/archaea_data/dense/GCA_001433455@Thermococcus_barophilus_CH5/TRG_match_matrix.tsv"
-GENE <- "TBCH5v1_RS00965"
-TREE <- "/home/eliott.tempez/Documents/archaea_data/whole_tree.nwk"
+trg_file <- "/home/eliott.tempez/Documents/archaea_data/dense/Thermococcus_barophilus_CH1/TRG_match_matrix.tsv"
+gene_name <- "TBCH1v1_0272.mRNA.0_elongated"
+tree <- "/home/eliott.tempez/Documents/archaea_data/whole_tree.nwk"
+focal_species = "Thermococcus_barophilus_CH1"
 
-gene_name <- paste0(GENE, ".mRNA.0_elongated")
+
 # Read the TRG file
-trg <- read.table(TRG_FILE, header = TRUE, sep = "\t", row.names = 1)
+trg <- read.table(trg_file, header = TRUE, sep = "\t", row.names = 1)
 # Keep the row corresponding to the gene of interest
 trg <- trg[gene_name, ]
-# Get a different color for each cell type
-colors <- sapply(trg, function(x) {
-  if (x == "CDS") {
-    "green"
-  } else if (grepl("gS", x)) {
-    "purple"
-  } else if (grepl("gNS", x)) {
-    "blue"
+trg <- t(trg)
+colnames(trg) <- "type"
+rownames(trg) <- gsub("\\.", "@", rownames(trg))
+for (i in seq_len(nrow(trg))) {
+  if (trg[i, "type"] == "CDS") {
+    trg[i, "type"] <- "CDS"
+  } else if (substr(trg[i, "type"], 1, 2) == "gS") {
+    trg[i, "type"] <- "gS"
+  } else if (substr(trg[i, "type"], 1, 3) == "gNS") {
+    trg[i, "type"] <- "gNS"
   } else {
-    "black"
+    trg[i, "type"] <- "no match"
   }
-})
+}
+
 
 # Read the tree
-tree <- read.tree(TREE)
+tree <- read.tree(tree)
 # Plot the tree
-p <- ggtree(tree) + geom_tiplab() + theme_tree2() + geom_point2(aes(color = colors), size = 2)
-print(p)
+p <- ggtree(tree, branch.length = "none") +
+  geom_tippoint(aes(subset = (label == focal_species)), shape = 23,
+                color = "black", fill = "yellow",
+                size = 3, stroke = 1, alpha = 1)
+p <- p + new_scale_fill()
+p <- gheatmap(p, data = trg, offset = 0.1, width = 0.1)
+p
