43c207b783225aad2d790e23fd7ebe8f7542bd82
eliott-tempez <tempez.el@gmail.com>
2025-04-01 14:53:03 +0200

    Add # in legend

--- scripts/11_calculate_conservation/map_gradient_tree.R
+++ scripts/11_calculate_conservation/map_gradient_tree.R
@@ -32,6 +32,11 @@ conservation_db_file <- paste0(conservation_db_dir,
 conservation_db <- read.table(conservation_db_file,
                               header = TRUE, sep = "\t",
                               row.names = 1)
+# Get number for each type
+n_denovo_base <- conservation_db[focal_species, "n_denovo"]
+n_trg_base <- conservation_db[focal_species, "n_trg"]
+n_intergenic_base <- conservation_db[focal_species, "n_intergenic"]
+n_cds_base <- conservation_db[focal_species, "n_cds"]
 # Get the ssearch result
 ssearch_db <- conservation_db[, c("ssearch_f0", "ssearch_f1", "ssearch_f2", "ssearch_f0_comp", "ssearch_f1_comp", "ssearch_f2_comp")]
 colnames(ssearch_db) <- c("+0 ", "+1 ", "+2 ", "-0 ", "-1 ", "-2 ")
@@ -66,25 +71,25 @@ p <- ggtree(tree, layout = "circular", branch.length = "none") +
 p <- p + new_scale_fill()
 p <- gheatmap(p, conservation_db_p[, "n_denovo", drop = FALSE],
               width = .05, colnames = FALSE) +
-  scale_fill_gradient(low = "white", high = "black", name = "De novo",
+  scale_fill_gradient(low = "white", high = "black", name = paste0("De novo\n(n = ", n_denovo_base, ")"),
                       guide = guide_colorbar(order = 1), limits = c(0, 100))
 ## TRGs ##
 p <- p + new_scale_fill()
 p <- gheatmap(p, conservation_db_p[, "n_trg", drop = FALSE], offset = 1,
               width = .05, colnames = FALSE) +
-  scale_fill_gradient(low = "white", high = "red", name = "TRGs",
+  scale_fill_gradient(low = "white", high = "red", name = paste0("TRGs\n(n = ", n_trg_base, ")"),
                       guide = guide_colorbar(order = 2), limits = c(0, 100))
 ## Noncoding ##
 p <- p + new_scale_fill()
 p <- gheatmap(p, conservation_db_p[, "n_intergenic", drop = FALSE], offset = 2,
               width = .05, colnames = FALSE) +
-  scale_fill_gradient(low = "white", high = "#6b00b2", name = "Intergenic",
+  scale_fill_gradient(low = "white", high = "#6b00b2", name = paste0("Noncoding\n(n = ", n_intergenic_base, ")"),
                       guide = guide_colorbar(order = 3), limits = c(0, 100))
 ## CDSs ##
 p <- p + new_scale_fill()
 p <- gheatmap(p, conservation_db_p[, "n_cds", drop = FALSE], offset = 3,
               width = .05, colnames = FALSE) +
-  scale_fill_gradient(low = "white", high = "#e67b00", name = "CDS",
+  scale_fill_gradient(low = "white", high = "#e67b00", name = paste0("CDSs\n(n = ", n_cds_base, ")"),
                       guide = guide_colorbar(order = 4), limits = c(0, 100))
 
 
