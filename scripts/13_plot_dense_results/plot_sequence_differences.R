8b75271516346c1223c77b9eb8eca66a057db455
eliott-tempez <tempez.el@gmail.com>
2025-04-07 11:19:55 +0200

    Plot sequence differences for all genes

--- scripts/13_plot_dense_results/plot_sequence_differences.R
+++ scripts/13_plot_dense_results/plot_sequence_differences.R
@@ -13,19 +13,18 @@ ggsave <- function(..., bg = "white",
 input_file <- "/home/eliott.tempez/Documents/M2_Stage_I2BC/results/13_plot_dense_results/sequences/sequence_features.csv"
 out_folder <- "/home/eliott.tempez/Documents/M2_Stage_I2BC/results/13_plot_dense_results/sequences"
 data <- read.table(input_file, header = TRUE, sep = "\t")
-n_cds <- nrow(data[data$type == "cds"])
-n_trg <- nrow(data[data$type == "trg"])
-n_denovo <- nrow(data[data$type == "denovo"])
+n_cds <- nrow(data[data$type == "cds", ])
+n_trg <- nrow(data[data$type == "trg", ])
+n_denovo <- nrow(data[data$type == "denovo", ])
 
 # Pivot to longer
-data$type <- data$origin
 data <- pivot_longer(data, cols = c("gc_content", "aromaticity", "instability", "mean_flexibility", "hydropathy", "len_nu"), names_to = "feature", values_to = "value")
 
 
 ##### Sequence length #####
-data_len <- data[data$feature == "gene_length", ]
-data_len$origin <- factor(data_len$origin, levels = c("cds", "trg", "denovo"))
-ggplot(data_len, aes(x = origin, y = value, fill = origin)) +
+data_len <- data[data$feature == "len_nu", ]
+data_len$type <- factor(data_len$type, levels = c("cds", "trg", "denovo"))
+ggplot(data_len, aes(x = type, y = value, fill = type)) +
   geom_boxplot(na.rm = TRUE, colour = "#2c2c2c") +
   labs(title = "Sequence length distribution",
        x = "Sequence type",
@@ -36,15 +35,18 @@ ggplot(data_len, aes(x = origin, y = value, fill = origin)) +
         axis.text.x = element_text(size = 16),
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
-        axis.text.y = element_text(size = 12))
+        axis.text.y = element_text(size = 12)) +
+  scale_x_discrete(labels = c("cds" = paste0("cds\n(n = ", n_cds, ")"),
+                              "trg" = paste0("trg\n(n = ", n_trg, ")"),
+                              "denovo" = paste0("denovo\n(n = ", n_denovo, ")")))
 ggsave(paste0(out_folder, "/sequence_length.png"))
 
 
 
 ##### GC content #####
-data_gc <- data[data$feature == "gc", ]
-data_gc$origin <- factor(data_gc$origin, levels = c("cds", "trg", "denovo"))
-ggplot(data_gc, aes(x = origin, y = value, fill = origin)) +
+data_gc <- data[data$feature == "gc_content", ]
+data_gc$type <- factor(data_gc$type, levels = c("cds", "trg", "denovo"))
+ggplot(data_gc, aes(x = type, y = value, fill = type)) +
   geom_boxplot(na.rm = TRUE, colour = "#2c2c2c") +
   labs(title = "GC content distribution",
        x = "Sequence type",
@@ -55,15 +57,18 @@ ggplot(data_gc, aes(x = origin, y = value, fill = origin)) +
         axis.text.x = element_text(size = 16),
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
-        axis.text.y = element_text(size = 12))
+        axis.text.y = element_text(size = 12)) +
+  scale_x_discrete(labels = c("cds" = paste0("cds\n(n = ", n_cds, ")"),
+                              "trg" = paste0("trg\n(n = ", n_trg, ")"),
+                              "denovo" = paste0("denovo\n(n = ", n_denovo, ")")))
 ggsave(paste0(out_folder, "/gc_content.png"))
 
 
 
 ##### Aromaticity #####
 data_aro <- data[data$feature == "aromaticity", ]
-data_aro$origin <- factor(data_aro$origin, levels = c("cds", "trg", "denovo"))
-ggplot(data_aro, aes(x = origin, y = value, fill = origin)) +
+data_aro$type <- factor(data_aro$type, levels = c("cds", "trg", "denovo"))
+ggplot(data_aro, aes(x = type, y = value, fill = type)) +
   geom_boxplot(na.rm = TRUE, colour = "#2c2c2c") +
   labs(title = "Aromaticity distribution",
        x = "Sequence type",
@@ -74,15 +79,18 @@ ggplot(data_aro, aes(x = origin, y = value, fill = origin)) +
         axis.text.x = element_text(size = 16),
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
-        axis.text.y = element_text(size = 12))
+        axis.text.y = element_text(size = 12)) +
+  scale_x_discrete(labels = c("cds" = paste0("cds\n(n = ", n_cds, ")"),
+                              "trg" = paste0("trg\n(n = ", n_trg, ")"),
+                              "denovo" = paste0("denovo\n(n = ", n_denovo, ")")))
 ggsave(paste0(out_folder, "/aromaticity.png"))
 
 
 
 ##### Instability index #####
 data_inst <- data[data$feature == "instability", ]
-data_inst$origin <- factor(data_inst$origin, levels = c("cds", "trg", "denovo"))
-ggplot(data_inst, aes(x = origin, y = value, fill = origin)) +
+data_inst$type <- factor(data_inst$type, levels = c("cds", "trg", "denovo"))
+ggplot(data_inst, aes(x = type, y = value, fill = type)) +
   geom_boxplot(na.rm = TRUE, colour = "#2c2c2c") +
   labs(title = "Instability index distribution",
        x = "Sequence type",
@@ -93,15 +101,18 @@ ggplot(data_inst, aes(x = origin, y = value, fill = origin)) +
         axis.text.x = element_text(size = 16),
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
-        axis.text.y = element_text(size = 12))
+        axis.text.y = element_text(size = 12)) +
+  scale_x_discrete(labels = c("cds" = paste0("cds\n(n = ", n_cds, ")"),
+                              "trg" = paste0("trg\n(n = ", n_trg, ")"),
+                              "denovo" = paste0("denovo\n(n = ", n_denovo, ")")))
 ggsave(paste0(out_folder, "/instability_index.png"))
 
 
 
 ##### Flexibility #####
-data_flex <- data[data$feature == "flexibility", ]
-data_flex$origin <- factor(data_flex$origin, levels = c("cds", "trg", "denovo"))
-ggplot(data_flex, aes(x = origin, y = value, fill = origin)) +
+data_flex <- data[data$feature == "mean_flexibility", ]
+data_flex$type <- factor(data_flex$type, levels = c("cds", "trg", "denovo"))
+ggplot(data_flex, aes(x = type, y = value, fill = type)) +
   geom_boxplot(na.rm = TRUE, colour = "#2c2c2c") +
   labs(title = "Mean flexibility distribution",
        x = "Sequence type",
@@ -112,14 +123,18 @@ ggplot(data_flex, aes(x = origin, y = value, fill = origin)) +
         axis.text.x = element_text(size = 16),
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
-        axis.text.y = element_text(size = 12))
+        axis.text.y = element_text(size = 12)) +
+  scale_x_discrete(labels = c("cds" = paste0("cds\n(n = ", n_cds, ")"),
+                              "trg" = paste0("trg\n(n = ", n_trg, ")"),
+                              "denovo" = paste0("denovo\n(n = ", n_denovo, ")")))
+ggsave(paste0(out_folder, "/mean_flexibility.png"))
 
 
 
 ##### Hydrophobicity #####
 data_hydro <- data[data$feature == "hydropathy", ]
-data_hydro$origin <- factor(data_hydro$origin, levels = c("cds", "trg", "denovo"))
-ggplot(data_hydro, aes(x = origin, y = value, fill = origin)) +
+data_hydro$type <- factor(data_hydro$type, levels = c("cds", "trg", "denovo"))
+ggplot(data_hydro, aes(x = type, y = value, fill = type)) +
   geom_boxplot(na.rm = TRUE, colour = "#2c2c2c") +
   labs(title = "Mean hydrophobicity distribution",
        x = "Sequence type",
@@ -130,5 +145,8 @@ ggplot(data_hydro, aes(x = origin, y = value, fill = origin)) +
         axis.text.x = element_text(size = 16),
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
-        axis.text.y = element_text(size = 12))
+        axis.text.y = element_text(size = 12)) +
+  scale_x_discrete(labels = c("cds" = paste0("cds\n(n = ", n_cds, ")"),
+                              "trg" = paste0("trg\n(n = ", n_trg, ")"),
+                              "denovo" = paste0("denovo\n(n = ", n_denovo, ")")))
 ggsave(paste0(out_folder, "/hydrophobicity.png"))
