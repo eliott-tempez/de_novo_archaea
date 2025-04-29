f8ff8dc62a59b3791973cb5dd7e7feb6eace4b4c
eliott-tempez <tempez.el@gmail.com>
2025-04-29 10:45:45 +0200

    Get plots with p-value

--- scripts/13_plot_dense_results/plot_sequence_differences.R
+++ scripts/13_plot_dense_results/plot_sequence_differences.R
@@ -2,7 +2,8 @@ library(ggplot2)
 library(tidyr)
 library(ggpubr)
 library(dplyr)
-library(rstatix)
+library(grid)
+
 ggsave <- function(..., bg = "white",
                    width = 1000, height = 1000,
                    units = "px", dpi = 100) {
@@ -14,10 +15,10 @@ ggsave <- function(..., bg = "white",
 }
 
 
-#input_file <- "/home/eliott.tempez/Documents/M2_Stage_I2BC/results/13_plot_dense_results/sequences/sequence_features_good_candidates_all.csv"
-#pval_file <- "/home/eliott.tempez/Documents/M2_Stage_I2BC/results/13_plot_dense_results/sequences/pvalues.tsv"
-input_file <- "/home/eltem/Documents/Cours/M2/Stage/M2_stage_I2BC/results/13_plot_dense_results/sequences/sequence_features_good_candidates.csv"
-pval_file <- "/home/eltem/Documents/Cours/M2/Stage/M2_stage_I2BC/results/13_plot_dense_results/sequences/pvalues.tsv"
+input_file <- "/home/eliott.tempez/Documents/M2_Stage_I2BC/results/13_plot_dense_results/sequences/sequence_features_good_candidates_all.csv"
+pval_file <- "/home/eliott.tempez/Documents/M2_Stage_I2BC/results/13_plot_dense_results/sequences/pvalues.tsv"
+#input_file <- "/home/eltem/Documents/Cours/M2/Stage/M2_stage_I2BC/results/13_plot_dense_results/sequences/sequence_features_good_candidates.csv"
+#pval_file <- "/home/eltem/Documents/Cours/M2/Stage/M2_stage_I2BC/results/13_plot_dense_results/sequences/pvalues.tsv"
 out_folder <- "/home/eliott.tempez/Documents/M2_Stage_I2BC/results/13_plot_dense_results/sequences"
 data <- read.table(input_file, header = TRUE, sep = "\t")
 n_cds <- nrow(data[data$type == "cds", ])
@@ -57,7 +58,6 @@ get_y_positions <- function(data, local_pvals, fact) {
   }
   reference_val <- max(whisker_tops) - min_whisker_base
   y_mat <- cbind("type" = types, "whisker_top" = whisker_tops)
-  print(y_mat)
   # Add y position
   pos <- max(as.numeric(y_mat[, "whisker_top"])) + reference_val * fact
   for (i in 1:3) {
@@ -85,7 +85,6 @@ get_pvals <- function(desc, data, fact) {
     local_pvals$p <= 0.05 ~ "*",
     TRUE ~ ""
   )
-  print(local_pvals)
   return(local_pvals)
 }
 
@@ -109,17 +108,18 @@ ggplot(data_len, aes(x = type, y = value, fill = type)) +
         axis.text.x = element_text(size = 16),
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
-        axis.text.y = element_text(size = 12)) +
+        axis.text.y = element_text(size = 12),
+        plot.title = element_text(hjust = 0.5)) +
   coord_trans(y = "log10") +
   scale_y_continuous(breaks = c(50, 100, 500, 1000, 2000),
                      labels = c("50", "100", "500", "1000", "2000")) +
   scale_x_discrete(labels = c("cds" = paste0("cds\n(n = ", n_cds, ")"),
                               "trg" = paste0("trg\n(n = ", n_trg, ")"),
                               "denovo" = paste0("denovo\n(n = ", n_denovo, ")"))) +
-  stat_pvalue_manual(get_pvals("length", data_len, 0.4), label = "p.signif", inherit.aes = FALSE, hide.ns = TRUE) +
-  annotate("text", x = 3.3, y = max(data_len$value) * 1.5,
+  stat_pvalue_manual(get_pvals("length", data_len, 0.5), label = "p.signif", inherit.aes = FALSE, hide.ns = TRUE) +
+  annotate("text", x = 3.3, y = max(data_len$value) * 1,
            label = "****: p <= 1e-5    ***: p <= 1e-4    **: p <= 1e-3    *: p <= 0.05", hjust = 1, vjust = 1, size = 3, color = "black")
-#ggsave(paste0(out_folder, "/sequence_length.png"))
+ggsave(paste0(out_folder, "/sequence_length.png"))
 
 
 
@@ -137,14 +137,15 @@ ggplot(data_gc, aes(x = type, y = value, fill = type)) +
         axis.text.x = element_text(size = 16),
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
-        axis.text.y = element_text(size = 12)) +
+        axis.text.y = element_text(size = 12),
+        plot.title = element_text(hjust = 0.5)) +
   scale_x_discrete(labels = c("cds" = paste0("cds\n(n = ", n_cds, ")"),
                               "trg" = paste0("trg\n(n = ", n_trg, ")"),
                               "denovo" = paste0("denovo\n(n = ", n_denovo, ")"))) +
   stat_pvalue_manual(get_pvals("gc_rate", data_gc, 0.1), label = "p.signif", inherit.aes = FALSE, hide.ns = "p") +
-  annotate("text", x = 3.3, y = max(data_gc$value) * 1.1,
+  annotate("text", x = 3.3, y = max(data_gc$value) * 1,
            label = "****: p <= 1e-5    ***: p <= 1e-4    **: p <= 1e-3    *: p <= 0.05", hjust = 1, vjust = 1, size = 3, color = "black")
-#ggsave(paste0(out_folder, "/gc_content.png"))
+ggsave(paste0(out_folder, "/gc_content.png"))
 
 
 
@@ -162,14 +163,15 @@ ggplot(data_aro, aes(x = type, y = value, fill = type)) +
         axis.text.x = element_text(size = 16),
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
-        axis.text.y = element_text(size = 12)) +
+        axis.text.y = element_text(size = 12),
+        plot.title = element_text(hjust = 0.5)) +
   scale_x_discrete(labels = c("cds" = paste0("cds\n(n = ", n_cds, ")"),
                               "trg" = paste0("trg\n(n = ", n_trg, ")"),
                               "denovo" = paste0("denovo\n(n = ", n_denovo, ")"))) +
   stat_pvalue_manual(get_pvals("aromaticity", data_aro, 0.1), label = "p.signif", inherit.aes = FALSE, hide.ns = "p") + 
-  annotate("text", x = 3.3, y = max(data_aro$value) * 1.1,
+  annotate("text", x = 3.3, y = max(data_aro$value) * .65,
            label = "****: p <= 1e-5    ***: p <= 1e-4    **: p <= 1e-3    *: p <= 0.05", hjust = 1, vjust = 1, size = 3, color = "black")
-#ggsave(paste0(out_folder, "/aromaticity.png"))
+ggsave(paste0(out_folder, "/aromaticity.png"))
 
 
 
@@ -187,14 +189,15 @@ ggplot(data_inst, aes(x = type, y = value, fill = type)) +
         axis.text.x = element_text(size = 16),
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
-        axis.text.y = element_text(size = 12)) +
+        axis.text.y = element_text(size = 12),
+        plot.title = element_text(hjust = 0.5)) +
   scale_x_discrete(labels = c("cds" = paste0("cds\n(n = ", n_cds, ")"),
                               "trg" = paste0("trg\n(n = ", n_trg, ")"),
                               "denovo" = paste0("denovo\n(n = ", n_denovo, ")"))) +
-  stat_pvalue_manual(get_pvals("instability", data_inst, 0.15), label = "p.signif", inherit.aes = FALSE, hide.ns = "p") +
-  annotate("text", x = 3.3, y = max(data_inst$value) * 1.1,
+  stat_pvalue_manual(get_pvals("instability", data_inst, 0.1), label = "p.signif", inherit.aes = FALSE, hide.ns = "p") +
+  annotate("text", x = 3.3, y = max(data_inst$value) * .65,
            label = "****: p <= 1e-5    ***: p <= 1e-4    **: p <= 1e-3    *: p <= 0.05", hjust = 1, vjust = 1, size = 3, color = "black")
-#ggsave(paste0(out_folder, "/instability_index.png"))
+ggsave(paste0(out_folder, "/instability_index.png"))
 
 
 
@@ -212,14 +215,15 @@ ggplot(data_flex, aes(x = type, y = value, fill = type)) +
         axis.text.x = element_text(size = 16),
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
-        axis.text.y = element_text(size = 12)) +
+        axis.text.y = element_text(size = 12),
+        plot.title = element_text(hjust = 0.5)) +
   scale_x_discrete(labels = c("cds" = paste0("cds\n(n = ", n_cds, ")"),
                               "trg" = paste0("trg\n(n = ", n_trg, ")"),
                               "denovo" = paste0("denovo\n(n = ", n_denovo, ")"))) +
-  stat_pvalue_manual(get_pvals("mean_flexibility", data_flex, 0.2), label = "p.signif", inherit.aes = FALSE, hide.ns = "p") +
-  annotate("text", x = 3.3, y = max(data_flex$value) * 1.05,
+  stat_pvalue_manual(get_pvals("mean_flexibility", data_flex, 0.1), label = "p.signif", inherit.aes = FALSE, hide.ns = "p") +
+  annotate("text", x = 3.3, y = max(data_flex$value) * 1.01,
            label = "****: p <= 1e-5    ***: p <= 1e-4    **: p <= 1e-3    *: p <= 0.05", hjust = 1, vjust = 1, size = 3, color = "black")
-#ggsave(paste0(out_folder, "/mean_flexibility.png"))
+ggsave(paste0(out_folder, "/mean_flexibility.png"))
 
 
 
@@ -237,11 +241,71 @@ ggplot(data_hydro, aes(x = type, y = value, fill = type)) +
         axis.text.x = element_text(size = 16),
         axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
-        axis.text.y = element_text(size = 12)) +
+        axis.text.y = element_text(size = 12),
+        plot.title = element_text(hjust = 0.5)) +
   scale_x_discrete(labels = c("cds" = paste0("cds\n(n = ", n_cds, ")"),
                               "trg" = paste0("trg\n(n = ", n_trg, ")"),
                               "denovo" = paste0("denovo\n(n = ", n_denovo, ")"))) +
   stat_pvalue_manual(get_pvals("hydropathy", data_hydro, 0.2), label = "p.signif", inherit.aes = FALSE, hide.ns = "p") +
-  annotate("text", x = 3.3, y = max(data_hydro$value) * 1.05,
+  annotate("text", x = 3.3, y = max(data_hydro$value) * .9,
            label = "****: p <= 1e-5    ***: p <= 1e-4    **: p <= 1e-3    *: p <= 0.05", hjust = 1, vjust = 1, size = 3, color = "black")
-#ggsave(paste0(out_folder, "/hydrophobicity.png"))
+ggsave(paste0(out_folder, "/hydrophobicity.png"))
+
+
+
+##### HCA #####
+data_hca <- data[data$feature == "hca", ]
+data_hca$type <- factor(data_hca$type, levels = c("cds", "trg", "denovo"))
+ggplot(data_hca, aes(x = type, y = value, fill = type)) +
+  geom_boxplot(na.rm = TRUE, colour = "#2c2c2c", outliers = FALSE) +
+  labs(title = "HCA distribution",
+       x = "Sequence type",
+       y = "HCA") +
+  scale_fill_manual(values = c("#cc7f0a", "#ad4646", "#4d4c4c")) +
+  theme_minimal() +
+  theme(legend.position = "none",
+        axis.text.x = element_text(size = 16),
+        axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14),
+        axis.text.y = element_text(size = 12),
+        plot.title = element_text(hjust = 0.5)) +
+  scale_x_discrete(labels = c("cds" = paste0("cds\n(n = ", n_cds, ")"),
+                              "trg" = paste0("trg\n(n = ", n_trg, ")"),
+                              "denovo" = paste0("denovo\n(n = ", n_denovo, ")"))) +
+  stat_pvalue_manual(get_pvals("hca", data_hca, 0.1), label = "p.signif", inherit.aes = FALSE, hide.ns = "p") +
+  annotate("text", x = 3.3, y = max(data_hca$value) * .95,
+           label = "****: p <= 1e-5    ***: p <= 1e-4    **: p <= 1e-3    *: p <= 0.05", hjust = 1, vjust = 1, size = 3, color = "black")
+ggsave(paste0(out_folder, "/hca.png"))
+
+
+
+##### AA use #####
+aa_use <- unique(data$feature)[endsWith(unique(data$feature), "_use")]
+aa_plots <- c()
+for (aa in aa_use) {
+  data_aa <- data[data$feature == aa, ]
+  data_aa$type <- factor(data_aa$type, levels = c("cds", "trg", "denovo"))
+  p <- ggplot(data_aa, aes(x = type, y = value, fill = type)) +
+    geom_boxplot(na.rm = TRUE, colour = "#2c2c2c", outliers = FALSE) +
+    labs(title = gsub("_use", "", aa),
+         x = "",
+         y = "") +
+    scale_fill_manual(values = c("#cc7f0a", "#ad4646", "#4d4c4c")) +
+    theme_minimal() +
+    theme(legend.position = "none",
+          plot.title = element_text(hjust = 0.5, face = "bold", size = 18),
+          axis.text.y = element_text(size = 16)) +
+    stat_pvalue_manual(get_pvals(aa, data_aa, 0.2), label = "p.signif", inherit.aes = FALSE, hide.ns = "p")
+  aa_plots <- c(aa_plots, list(p))
+}
+fig <- ggarrange(plotlist = aa_plots, ncol = 5, nrow = 4)
+annotate_figure(fig, bottom = text_grob("Sequence type\n", size = 14),
+                left = text_grob("Amino-acid use (%)", rot = 90, size = 14),
+                right = text_grob("****: p <= 1e-5    ***: p <= 1e-4    **: p <= 1e-3    *: p <= 0.05\n", size = 8, rot = -90), 
+                top = text_grob("Amino-acid use distribution", size = 18))
+ggsave(paste0(out_folder, "/aa_use.png"), width = 1200, height = 1200)
+
+
+
+
+
