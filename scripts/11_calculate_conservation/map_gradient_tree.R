d8336f7497f238caae43bdbced16306cba056591
eliott-tempez <tempez.el@gmail.com>
2025-04-29 16:01:38 +0200

    Fix typo

--- scripts/11_calculate_conservation/map_gradient_tree.R
+++ scripts/11_calculate_conservation/map_gradient_tree.R
@@ -83,7 +83,7 @@ p <- gheatmap(p, conservation_db_p[, "n_trg", drop = FALSE], offset = 1,
 p <- p + new_scale_fill()
 p <- gheatmap(p, conservation_db_p[, "n_intergenic", drop = FALSE], offset = 2,
               width = .05, colnames = FALSE) +
-  scale_fill_gradient(low = "white", high = "#6b00b2", name = paste0("Noncoding\n(n = ", n_intergenic_base, ")"),
+  scale_fill_gradient(low = "white", high = "#6b00b2", name = paste0("Intergenic\n(n = ", n_intergenic_base, ")"),
                       guide = guide_colorbar(order = 3), limits = c(0, 100))
 ## CDSs ##
 p <- p + new_scale_fill()
@@ -108,51 +108,51 @@ ggsave(paste0(output_dir, "conservation_map_", focal_species, ".png"))
 
 ######## SSEARCH ########
 # Plot the tree
-p <- ggtree(tree, layout = "circular", branch.length = "none") +
-  geom_tippoint(aes(subset = (label == focal_species)), shape = 23,
-                color = "black", fill = "yellow",
-                size = 3, stroke = 1, alpha = 1)
+#p <- ggtree(tree, layout = "circular", branch.length = "none") +
+#  geom_tippoint(aes(subset = (label == focal_species)), shape = 23,
+#                color = "black", fill = "yellow",
+#                size = 3, stroke = 1, alpha = 1)
 # Add each reading frame in the same heatmap color
 ## Frame 0 ##
-p <- p + new_scale_fill()
-p <- gheatmap(p, ssearch_db_p[, "+0 ", drop = FALSE],
-              width = .05, colnames = TRUE, colnames_angle = 90, font.size = 3) +
-  scale_fill_gradient(low = "white", high = "#e67b00", name = "sens",
-                      guide = guide_colorbar(order = 1), limits = c(0, 100))
+#p <- p + new_scale_fill()
+#p <- gheatmap(p, ssearch_db_p[, "+0 ", drop = FALSE],
+#              width = .05, colnames = TRUE, colnames_angle = 90, font.size = 3) +
+#  scale_fill_gradient(low = "white", high = "#e67b00", name = "sens",
+#                      guide = guide_colorbar(order = 1), limits = c(0, 100))
 ## Frame 1 ##
-p <- p + new_scale_fill()
-p <- gheatmap(p, ssearch_db_p[, "+1 ", drop = FALSE], offset = 1,
-              width = .05, colnames = TRUE, colnames_angle = 90, font.size = 3) +
-  scale_fill_gradient(low = "white", high = "#e67b00",
-                      guide = "none", limits = c(0, 100))
+#p <- p + new_scale_fill()
+#p <- gheatmap(p, ssearch_db_p[, "+1 ", drop = FALSE], offset = 1,
+#              width = .05, colnames = TRUE, colnames_angle = 90, font.size = 3) +
+#  scale_fill_gradient(low = "white", high = "#e67b00",
+#                      guide = "none", limits = c(0, 100))
 ## Frame 2 ##
-p <- p + new_scale_fill()
-p <- gheatmap(p, ssearch_db_p[, "+2 ", drop = FALSE], offset = 2,
-              width = .05, colnames = TRUE, colnames_angle = 90, font.size = 3) +
-  scale_fill_gradient(low = "white", high = "#e67b00",
-                      guide = "none", limits = c(0, 100))
+#p <- p + new_scale_fill()
+#p <- gheatmap(p, ssearch_db_p[, "+2 ", drop = FALSE], offset = 2,
+#              width = .05, colnames = TRUE, colnames_angle = 90, font.size = 3) +
+#  scale_fill_gradient(low = "white", high = "#e67b00",
+#                      guide = "none", limits = c(0, 100))
 ## Frame -0 ##
-p <- p + new_scale_fill()
-p <- gheatmap(p, ssearch_db_p[, "-0 ", drop = FALSE], offset = 3,
-              width = .05, colnames = TRUE, colnames_angle = 90, font.size = 3) +
-  scale_fill_gradient(low = "white", high = "#009E73", name = "antisens",
-                      guide = guide_colorbar(order = 2), limits = c(0, 100))
+#p <- p + new_scale_fill()
+#p <- gheatmap(p, ssearch_db_p[, "-0 ", drop = FALSE], offset = 3,
+#              width = .05, colnames = TRUE, colnames_angle = 90, font.size = 3) +
+#  scale_fill_gradient(low = "white", high = "#009E73", name = "antisens",
+#                      guide = guide_colorbar(order = 2), limits = c(0, 100))
 ## Frame -1 ##
-p <- p + new_scale_fill()
-p <- gheatmap(p, ssearch_db_p[, "-1 ", drop = FALSE], offset = 4,
-              width = .05, colnames = TRUE, colnames_angle = 90, font.size = 3) +
-  scale_fill_gradient(low = "white", high = "#009E73",
-                      guide = "none", limits = c(0, 100))
+#p <- p + new_scale_fill()
+#p <- gheatmap(p, ssearch_db_p[, "-1 ", drop = FALSE], offset = 4,
+#              width = .05, colnames = TRUE, colnames_angle = 90, font.size = 3) +
+#  scale_fill_gradient(low = "white", high = "#009E73",
+#                      guide = "none", limits = c(0, 100))
 ## Frame -2 ##
-p <- p + new_scale_fill()
-p <- gheatmap(p, ssearch_db_p[, "-2 ", drop = FALSE], offset = 5,
-              width = .05, colnames = TRUE, colnames_angle = 90, font.size = 3) +
-  scale_fill_gradient(low = "white", high = "#009E73",
-                      guide = "none", limits = c(0, 100))
+#p <- p + new_scale_fill()
+#p <- gheatmap(p, ssearch_db_p[, "-2 ", drop = FALSE], offset = 5,
+#              width = .05, colnames = TRUE, colnames_angle = 90, font.size = 3) +
+#  scale_fill_gradient(low = "white", high = "#009E73",
+#                      guide = "none", limits = c(0, 100))
 
 # Add the title
-p <- p + ggtitle(paste0("Conservation rate (%) for the 6 reading frames\n", focal_species)) +
-  theme(plot.title = element_text(hjust = 0.5, vjust = -10))
+#p <- p + ggtitle(paste0("Conservation rate (%) for the 6 reading frames\n", focal_species)) +
+#  theme(plot.title = element_text(hjust = 0.5, vjust = -10))
 
 # Save the image
-ggsave(paste0(output_dir, "conservation_map_frames_", focal_species, ".png"))
+#ggsave(paste0(output_dir, "conservation_map_frames_", focal_species, ".png"))
