b542d1cf613cc2052782ea0857391d73aaa995c4
eliott-tempez <tempez.el@gmail.com>
2025-06-04 14:47:13 +0200

    Plot venn diagram for furthest outgroup

--- scripts/14_get_noncoding_match/analyse_noncoding_origin_frames.R
+++ scripts/14_get_noncoding_match/analyse_noncoding_origin_frames.R
@@ -1,4 +1,6 @@
 library(dplyr)
+library(ggVennDiagram)
+library(ggplot2)
 
 ggsave <- function(..., bg = "white",
                    width = 1000, height = 1000,
@@ -20,6 +22,63 @@ data <- read.table(input_file, header = TRUE, sep = "\t")
 # Remove columns that have 3 codons or less
 data[, c("intergenic", "f0", "f1", "f2")] <- lapply(data[, c("intergenic", "f0", "f1", "f2")], function(x) ifelse(x < 10, 0, x))
 
+
+
+##### Origin of the furthest outgroup #####
+# Compute furthest_outgroup for each gene_id
+furthest_outgroup_df <- data %>%
+  group_by(gene_id) %>%
+  summarise(furthest_outgroup = max(outgroup_nb, na.rm = TRUE)) %>%
+  ungroup()
+# Add furthest_outgroup column to data
+data <- data %>%
+  left_join(furthest_outgroup_df, by = "gene_id")
+# Keep only the furthest outgroup for each gene_id
+furthest_outgroup_data <- data %>%
+  rowwise() %>%
+  filter(outgroup_nb == furthest_outgroup) %>%
+  ungroup() %>%
+  # Keep only 1 row per gene_id
+  distinct(gene_id, .keep_all = TRUE)
+
+
+## Venn diagram
+intergenic_genes <- furthest_outgroup_data %>%
+  filter(intergenic > 0) %>%
+  pull(gene_id)
+f0_genes <- furthest_outgroup_data %>%
+  filter(f0 > 0) %>%
+  pull(gene_id)
+f1_genes <- furthest_outgroup_data %>%
+  filter(f1 > 0) %>%
+  pull(gene_id)
+f2_genes <- furthest_outgroup_data %>%
+  filter(f2 > 0) %>%
+  pull(gene_id)
+venn_list = list(
+  intergenic = intergenic_genes,
+  f0 = f0_genes,
+  f1 = f1_genes,
+  f2 = f2_genes
+)
+# Plot
+ggVennDiagram(venn_list, label_alpha = 0) +
+  labs(title = "Origin of the furthest outgroup for de novo genes in NC match") +
+  theme(legend.position = "none") +
+    scale_fill_gradientn(
+    colors = c("white", "#ffbec8", "#ff7070"),
+    values = scales::rescale(c(0, 7, 54)))
+
+
+
+
+
+
+
+
+
+
+##### Analyse all NC matches #####
 # Extract the genes which don't have the same origin across all neighbours
 data_origin <- data %>%
   group_by(gene_id) %>%
@@ -32,12 +91,23 @@ data_origin <- data %>%
   )
 print("Genes with different origins across neighbours:")
 different_origin <- data_origin %>%
-  filter(!(intergenic %in% c(0, n)) |
-           !(f0 %in% c(0, n)) |
-           !(f1 %in% c(0, n)) |
-           !(f2 %in% c(0, n)))
+  rowwise() %>%
+  filter(
+    !(intergenic %in% c(0, n)) |
+      !(f0 %in% c(0, n)) |
+      !(f1 %in% c(0, n)) |
+      !(f2 %in% c(0, n))
+  ) %>%
+  ungroup()
+print(different_origin)
+
 same_origin <- data_origin %>%
-  filter(intergenic %in% c(0, n) &
-           f0 %in% c(0, n) &
-           f1 %in% c(0, n) &
-           f2 %in% c(0, n))
+  rowwise() %>%
+  filter(
+    intergenic %in% c(0, n) &
+      f0 %in% c(0, n) &
+      f1 %in% c(0, n) &
+      f2 %in% c(0, n)
+  ) %>%
+  ungroup()
+
