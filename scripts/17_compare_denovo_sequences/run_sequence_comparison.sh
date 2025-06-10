5f4ba211f602c56716623be2677b570923198fe7
eliott-tempez <tempez.el@gmail.com>
2025-06-10 17:31:22 +0200

    fix typo

--- scripts/17_compare_denovo_sequences/run_sequence_comparison.sh
+++ scripts/17_compare_denovo_sequences/run_sequence_comparison.sh
@@ -12,7 +12,7 @@
 SCRIPTS=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/stage/M2_stage_I2BC/scripts
 GOOD_CANDIDATES=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/stage/M2_stage_I2BC/results/14_get_noncoding_match/good_candidates.txt
 IORF_FILE=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/stage/M2_stage_I2BC/results/17_compare_denovo_sequences/iorfs.txt
-INTERGENIC_GC=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/stage/M2_stage_I2BC/10_analyse_intergenic/intergenic_gc.tsv
+INTERGENIC_GC=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/stage/M2_stage_I2BC/results/10_analyse_intergenic/intergenic_gc.tsv
 CONTAINER=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/programs/ORFmine/orfmine_latest.sif
 IUPRED_DIR=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/programs/iupred
 TANGO=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/programs/tango/tango_x86_64_release
