0104526365c030cd2c09818fec080f9ac16b0508
eliott-tempez <tempez.el@gmail.com>
2025-06-17 19:05:09 +0200

    Run pvals one-sided with 1 bin

--- scripts/17_compare_denovo_sequences/run_sequence_pvals.sh
+++ scripts/17_compare_denovo_sequences/run_sequence_pvals.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
 #SBATCH -p common
-#SBATCH -J pval_descriptors
+#SBATCH -J pval_1_bin
 #SBATCH --time=1000-00
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks=1
 #SBATCH --mem=32gb
-#SBATCH -o /home/eliott.tempez/pval_comparison_output_2.log
-#SBATCH -e /home/eliott.tempez/pval_comparison_error_2.log
+#SBATCH -o /home/eliott.tempez/pval_1_bin_out.log
+#SBATCH -e /home/eliott.tempez/pval_1_bin_err.log
 
 SCRIPTS=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/stage/M2_stage_I2BC/scripts
 GOOD_CANDIDATES=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/stage/M2_stage_I2BC/results/14_get_noncoding_match/good_candidates.txt
@@ -15,8 +15,8 @@ DESCRIPTORS=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/archaea_data/pvals_descript
 FA_DIR=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/archaea_data/complete_122/fasta_renamed
 DENSE_DIR=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/archaea_data/dense
 OUT_DIR=/store/EQUIPES/BIM/MEMBERS/eliott.tempez/archaea_data/pvals_descriptors
-OUTPUT_LOG=/home/eliott.tempez/pval_comparison_output_2.log
-ERROR_LOG=/home/eliott.tempez/pval_comparison_error_2.log
+OUTPUT_LOG=/home/eliott.tempez/pval_1_bin_out.log
+ERROR_LOG=/home/eliott.tempez/pval_1_bin_err.log
 
 
 # Set up a trap to clean up on exit or interruption
@@ -39,7 +39,7 @@ cp $GOOD_CANDIDATES .
 echo "Environment created" >> $OUTPUT_LOG
 
 # Run script
-python 17_compare_denovo_sequences/calculate_pvals_good_bad_denovo.py >> $OUTPUT_LOG
+python 17_compare_denovo_sequences/calculate_pvals_seq_comparison.py >> $OUTPUT_LOG
 
 # Get output
 cp pvalues*.csv $OUT_DIR
