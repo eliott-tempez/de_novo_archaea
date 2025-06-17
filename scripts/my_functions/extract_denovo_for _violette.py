4fe977fd6280010fe505f4deb312b12b18f0ee49
eliott-tempez <tempez.el@gmail.com>
2025-06-17 19:18:04 +0200

    Extract the new sequences for the 64 good denovo

--- scripts/my_functions/extract_denovo_for _violette.py
+++ scripts/my_functions/extract_denovo_for _violette.py
@@ -5,7 +5,7 @@ import os
 from my_functions.paths import DENSE_DIR, GENOMES_LIST, CDS_DIR, GFF_DIR, FA_DIR
 from my_functions.genomic_functions import extract_denovo_info
 
-
+good_fasta = "/home/eliott.tempez/Documents/M2_Stage_I2BC/results/18_clustering/good_candidates.fasta"
 all_denovo_file = "/home/eliott.tempez/Documents/M2_Stage_I2BC/results/14_get_noncoding_match/all_denovo.txt"
 good_denovo_file = "/home/eliott.tempez/Documents/M2_Stage_I2BC/results/14_get_noncoding_match/good_candidates.txt"
 out_folder = "/home/eliott.tempez/Documents/M2_Stage_I2BC/results/my_functions"
@@ -36,7 +36,6 @@ for genome in genomes:
     local_dict = extract_denovo_info(genome)
     for denovo in local_dict:
         local_dict[denovo]["genome"] = genome
-    
     all_denovo_dict.update(local_dict)
 
 # Write all denovo info to file
@@ -64,4 +63,8 @@ with open(os.path.join(out_folder, "discarded_denovo.tsv"), "w") as f:
         f.write(f"{denovo}\t{genome}\t{contig}\t{start}\t{end}\t{strand}\n")
 
 
-
+# Write fasta file
+with open(good_fasta, "w") as f:
+    for denovo in good_denovo:
+        seq = all_denovo_dict[denovo]["sequence"]
+        f.write(f">{denovo}\n{seq}\n")
